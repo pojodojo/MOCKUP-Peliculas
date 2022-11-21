ff7492a5e8c61f79f7613de4e38de4177be70e32
andre <andre@ACE-DELL-G3>
2022-11-21 10:39:57 -0500

    Agregar archivos de proyecto.

---
+++ MOCKUP-Peliculas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MOCKUP_Peliculas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
